ac8af29cd0562f317949e4dd246d3ac082a608d0
Nahum Alon <nochum.shepsl@gmail.com>
2024-11-26 23:16:25 +0200

    Method main doesn't need keyword "throws"

--- src/main/java/telran/multithreading/Main.java
+++ src/main/java/telran/multithreading/Main.java
@@ -4,7 +4,7 @@ public class Main {
     static final Object m1 = new Object();
     static final Object m2 = new Object();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         ThreadRuner t1 = new ThreadRuner("source 1", m1, "source 2", m2);
         ThreadRuner t2 = new ThreadRuner("source 2", m2, "source 1", m1);
         t1.start();
